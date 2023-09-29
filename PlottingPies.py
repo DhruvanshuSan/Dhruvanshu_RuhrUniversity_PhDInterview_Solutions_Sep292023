6baa1699743a453d1dd6550720cfffa5dacb6775
Dhruvanshu Parmar <82689754+DhruvanshuSan@users.noreply.github.com>
2023-09-29 18:39:23 +0530

    Update PlottingPies.py

--- PlottingPies.py
+++ PlottingPies.py
@@ -8,23 +8,23 @@ import math
 
 def PiePerc(lab):
 	redarray = np.unique(lab)
-	if len(redarray) == 2:
+	if len(redarray) == 2:	#This ensures we have two distinct quarks
 		a = stats.mode(lab)
 		#print(redarray,a[0][0])
 		if redarray[0] == a[0][0]:
 			percarray=[200/3,100/3]
 		else:
 			percarray=[100/3,200/3]
-	if len(redarray) == 3:
-		if redarray[2] == 6:
+	if len(redarray) == 3: #This ensures we have three distinct quarks
+		if redarray[2] == 6: #Meson
 			percarray = [100/2,100/2]
 			redarray = [redarray[0],redarray[1]]
-		else:
+		else: #Baryon
 			percarray = [100/3,100/3,100/3]
 	return percarray,redarray
 
 plt.figure()
-A,B,C,D,E,F = np.loadtxt("BaryonsList.txt",unpack=True,dtype=str,delimiter=",")
+A,B,C,D,E,F = np.loadtxt("BaryonsList.txt",unpack=True,dtype=str,delimiter=",") #ParticleName,Quark1Code,Quark2Code,Quark3Code,Lifetime(in 10^-13 sec),ParticleCode(0 for Baryon, 1 for Meson)
 fig, ax = plt.subplots(nrows=2,ncols=5,figsize=(15,10))
 fig.suptitle("Quark composition of charmed hadrons",fontsize='40')
 hl = int(len(A)/2)
@@ -32,8 +32,8 @@ Radii = np.sqrt(E.astype(np.float64))
 SF = 0.3/np.min(Radii)
 MaxRadii = np.max(Radii)
 patch_handles=[]
-labelslist = ['u','d','c','s','b','t']
-colorslist = ['red','green','blue','orange','magenta','yellow']
+labelslist = ['u','d','c','s','b','t'] #Quarks (The position in array corresponds to quarkcode. A QuarkCode of 6 is used just to ensure numpy doesnt fail while calling elements
+colorslist = ['red','green','blue','orange','magenta','yellow'] #Associated color to a particular quark in piechart. Helpful in keeping track of the corresponding quark
 for i in range(len(A)):
 	print(A[i])
 	inputlabels = [int(B[i]),int(C[i]),int(D[i])]
